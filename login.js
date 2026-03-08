650a49279c425d3a6bf54de3c929d345235f170c
Safa-Anan08 <safaspractice@gmail.com>
2026-03-08 23:39:26 +0600

    login UI

---
+++ login.js
@@ -0,0 +1,19 @@
+document.getElementById("loginBtn")
+.addEventListener("click",function(){
+
+const username=document.getElementById("username").value
+const password=document.getElementById("password").value
+
+if(username==="admin" && password==="admin123"){
+
+localStorage.setItem("login","true")
+
+window.location.href="dashboard.html"
+
+}else{
+
+alert("Invalid Login")
+
+}
+
+})
